63ec315b5e7909cc3890ea816415d52c08d59007
fromlabo <fromlabo@fromlabonoMacBook-Air.local>
2021-03-04 21:57:19 +0900

    途中提出4

--- top.php
+++ top.php
@@ -15,35 +15,31 @@
       <label>BuzzNum: <label>
       <input type="text" name="buzz"><br>
     </div>
-    <input type="submit" value="実行">
+    <input type="submit" name="start" value="実行">
   </form>
   <?php
     function fizzbuzz() {
+      $fizz = $_POST[fizz];
+      $buzz = $_POST[buzz];
       echo '<h2>' .【出力】 . '</h2>';
-      if(empty($_POST[fizz] || $_POST[buzz])){
+      if(empty($fizz || $buzz)){
         echo '値を入力してください。';
+      }else if(!preg_match("/[0-9]/",$fizz) || !preg_match("/[0-9]/",$buzz)){
+        echo '数字を入力してください';
       }else{
-        $fizz = $_POST[fizz];
-        $buzz = $_POST[buzz];
-        if(!preg_match("/[0-9]+$/",$fizz)){
-          echo '数字を入力してください';
-        }else if(!preg_match("/[0-9]+$/",$buzz)){
-          echo '数字を入力してください';
-        }else{
-          for($i = 0; $fizzResult < 99 && $buzzResult < 99; $i++) {
-            if($fizz % $fizz === 0) {
-              $fizzResult = $fizzResult + $fizz;
-              echo 'Fizz ' . $fizzResult . '<br>';
-            }
-            if(($buzz % $buzz === 0)) {
-              $buzzResult = $buzzResult + $buzz;
-              echo 'Buzz ' . $buzzResult . '<br>';
-            }
+        for($i = 0; $fizzResult < 99 && $buzzResult < 99; $i++) {
+          if($fizz % $fizz === 0) {
+            $fizzResult = $fizzResult + $fizz;
+            echo 'Fizz ' . $fizzResult . '<br>';
+          }
+          if(($buzz % $buzz === 0)) {
+            $buzzResult = $buzzResult + $buzz;
+            echo 'Buzz ' . $buzzResult . '<br>';
           }
         }
       }
     }
-    if(!empty($_POST[fizz] && $_POST[buzz])) {
+    if(!empty($_POST[start])) {
       fizzbuzz();
     }
   ?>
